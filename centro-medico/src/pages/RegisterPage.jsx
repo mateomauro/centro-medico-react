b53a381983ff46442bb2c36be978466a7738430c
Tomas4321 <tomasdonati8@gmail.com>
2025-01-30 13:04:08 -0300

    Agregamos validaciones de errores

--- centro-medico/src/pages/RegisterPage.jsx
+++ centro-medico/src/pages/RegisterPage.jsx
@@ -35,23 +35,36 @@ const RegisterPage = () => {
 
     const validateForm = () => {
         let isValid = true;
-
+        let newErrors = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        };
+    
         if (!formData.firstName) {
             isValid = false;
+            newErrors.firstName = "El nombre no es válido";
         }
         if (!formData.lastName) {
             isValid = false;
+            newErrors.lastName = "El apellido no es válido";
         }
         if (!formData.email || !/\S+@\S+\.\S+/.test(formData.email)) {
             isValid = false;
+            newErrors.email = "El email no es válido";
         }
         if (!formData.password) {
             isValid = false;
+            newErrors.password = "La contraseña no es válida";
         }
         if (formData.password !== confirmPassword) {
             isValid = false;
+            newErrors.confirmPassword = "Las contraseñas no coinciden";
         }
-
+    
+        setErrors(newErrors);
         return isValid;
     };
 
@@ -94,22 +107,27 @@ const RegisterPage = () => {
                 <div className='input-register'>
                     <label>Nombre</label>
                     <input type="text" name='firstName' value={formData.firstName} onChange={handleChange} />
+                    {errors.firstName && <span className='error'>{errors.firstName}</span>}
                 </div>
                 <div className='input-register'>
                     <label>Apellido</label>
                     <input type="text" name='lastName' value={formData.lastName} onChange={handleChange} />
+                    {errors.lastName && <span className='error'>{errors.lastName}</span>}
                 </div>
                 <div className='input-register'>
                     <label>Email</label>
                     <input type="email" name="email" value={formData.email} onChange={handleChange} />
+                    {errors.email && <span className='error'>{errors.email}</span>}
                 </div>
                 <div className='input-register'>
                     <label>Contraseña</label>
                     <input type="password" name="password" value={formData.password} onChange={handleChange} />
+                    {errors.password && <span className='error'>{errors.password}</span>}
                 </div>
                 <div className='input-register'>
                     <label>Confirmar contraseña</label>
                     <input type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPasswordChange} />
+                    {errors.confirmPassword && <span className='error'>{errors.confirmPassword}</span>}
                 </div>
                 <button type="submit">Registrarme</button>
             </form>
